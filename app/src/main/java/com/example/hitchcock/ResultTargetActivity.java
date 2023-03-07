4b5e4097194da68722dba32ae3e99d8a88c0937b
Ela Wieczorek <elizabeth01wieczorek@gmail.com>
2023-03-07 12:21:08 -0500

    Finished all calculations. Few issues with xml layout. May need to add an additional intent to push result String onto new page with cited sources.

---
+++ app/src/main/java/com/example/hitchcock/ResultTargetActivity.java
@@ -0,0 +1,9 @@
+package com.example.hitchcock;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultTargetActivity extends AppCompatActivity {
+//Attempt to get results from calculations in CalculateTargetActivity.java to be displayed here
+    //with intents.
+}
